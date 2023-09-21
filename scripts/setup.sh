ebac8520f2c6345d72ea6725a9edaf16d74b5b2d
Devon Frind <devon.fri.2000@gmail.com>
2023-09-21 03:13:54 -0700

    Add config file fetching and update vscode tasks

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,6 +14,11 @@ function install() {
 
     echo "Adding OS161 dependencies to PATH..."
     echo "PATH=$HOME/tools/os161/bin:$HOME/tools/sys161/bin:$PATH >> $HOME/.bashrc"
+
+    echo "Fetching default sys161.conf file from the ECE server..."
+    wget people.ece.ubc.ca/~os161/download/sys161.conf.sample -O ${WORKSPACE_DIR}/os161/sys161.conf
+    echo "You may copy sys161.conf located in ${WORKSPACE_DIR}/os161 into your root directory or use your own with:"
+    echo -e "\tcp \$WORKSPACE_DIR/os161/sys161.conf \$WORKSPACE_DIR/os161/root"
 }
 
 if [[ -d $HOME/tools ]]; then
