c56577ff05b799573dcb078ae49b7d2299b2375c
Devon Frind <devon.fri.2000@gmail.com>
2023-09-25 14:31:55 -0700

    Automate sys161.conf file addition to root

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,3 @@
-
 #/bin/bash
 
 # This script only needs to be ran once even after rebuilding (as long as the osdev_home volume isn't deleted)
@@ -16,6 +15,17 @@ if [[ -d $HOME/tools ]]; then
     echo "~/tools already exists. Delete ~/tools if rebuilding is desired. Skipping build..."
 else
     install
+fi
+
+if [[ ! -d ${WORKSPACE_DIR}/os161/root ]]; then
+    mkdir -p ${WORKSPACE_DIR}/os161/root
+fi
+
+if [[ ! -f ${WORKSPACE_DIR}/os161/root/sys161.conf ]]; then
+    echo "sys161.conf does not exist in root directory. Adding config file..."
+    cp ${HOME}/sys161.conf ${WORKSPACE_DIR}/os161/root
+else
+    echo "sy161.conf already exists"
 fi
 
 echo "Done setup!"
