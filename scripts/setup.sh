8873e7fdc3906c97744fbee9a0506842b5e3391d
Devon Frind <devon.fri.2000@gmail.com>
2023-09-25 14:26:36 -0700

    Remove sys161 config fetching from setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,13 +9,7 @@ function install() {
     prev_dir=$(pwd)
     cd $WORKSPACE_DIR/scripts
     bash ./cs161-ubuntu-darwin.sh
-
     cd $prev_dir
-
-    echo "Fetching default sys161.conf file from the ECE server..."
-    wget people.ece.ubc.ca/~os161/download/sys161.conf.sample -O ${WORKSPACE_DIR}/os161/sys161.conf
-    echo "You may copy sys161.conf (or use your own) located in ${WORKSPACE_DIR}/os161 into your root directory with:"
-    echo -e "\tcp \$WORKSPACE_DIR/os161/sys161.conf \$WORKSPACE_DIR/os161/root"
 }
 
 if [[ -d $HOME/tools ]]; then
