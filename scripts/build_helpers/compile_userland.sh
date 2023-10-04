4d0e53afa96f4e3bf3d9cc54b250c2a15ca11fd0
Devon Friend <69879126+DFriend01@users.noreply.github.com>
2023-10-04 14:27:45 -0700

    Update scripts and vscode tasks (#2)
    * Add clean build script and task
    
    * Add build scripts
    
    * Add help messages and CLI arguments to build helpers
    
    * Implement full build script
    
    * Tweak argument configurations
    
    * Update VS Code tasks to use new build scripts when possible
    
    * Update echo statements in setup script
    
    * Check for valid kernel in the build script

---
+++ scripts/build_helpers/compile_userland.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+function helpMessage() {
+    echo "Usage: ./compile_userland.sh"
+    echo "Compile userland"
+}
+
+# Grab CLI arguments
+while getopts ":h" flag; do
+    case ${flag} in
+        h) helpMessage; exit 0 ;;
+        \?) echo "Invalid option: -${OPTARG}"; helpMessage; exit 1 ;;
+        *) echo "Unhandled option: -${OPTARG}"; helpMessage; exit 1 ;;
+    esac
+done
+
+prev_dir=$(pwd)
+if [[ -d ${OS161_SRC} ]]; then
+    cd ${OS161_SRC}
+    bmake -j$(nproc)
+    bmake install
+else
+    echo "Cannot compile userland because ${OS161_SRC} does not exist."
+fi
+
+cd ${prev_dir}
+echo "Done compiling userland"
---
+++ scripts/build_helpers/configure_and_compile_kernel.sh
@@ -0,0 +1,54 @@
+#/bin/bash
+
+function helpMessage() {
+    echo "Usage: ./configure_and_compile_kernel.sh [OPTIONS]..."
+    echo "Configures and compiles a kernel"
+    echo "Example: ./configure_and_compile_kernel.sh -k DUMBVM"
+    echo ""
+    echo "Options"
+    echo -e "\t-k: The kernel to configure and compile. Must be one of:" \
+            "DUMBVM, DUMBVM-OPT, GENERIC, GENERIC-OPT, SYNCHPROBS"
+    echo -e "\t-h: Display this message"
+}
+
+kernel=""
+
+# Grab CLI arguments
+while getopts ":hk:" flag; do
+    case ${flag} in
+        h) helpMessage; exit 0 ;;
+        k) kernel=${OPTARG} ;;
+        \?) echo "Invalid option: -${OPTARG}"; helpMessage; exit 1 ;;
+        *) echo "Unhandled option: -${OPTARG}"; helpMessage; exit 1 ;;
+    esac
+done
+
+# Assert that the kernel argument is non-empty
+if [[ -z ${kernel} ]]; then
+    echo "ERROR: Option -k is missing or argument value is empty"
+    helpMessage
+    exit 1
+fi
+
+prev_dir=$(pwd)
+kernel_conf_dir=${OS161_SRC}/kern/conf
+kernel_dir=${OS161_SRC}/kern/compile/${kernel}
+
+if [[ -d ${kernel_conf_dir} && ! -z ${kernel} ]]; then
+    cd ${kernel_conf_dir}
+    ./config ${kernel}
+else
+    echo "Cannot configure kernel ${kernel} because ${kernel_dir} does not exist."
+fi
+
+if [[ -d ${kernel_dir} && ! -z ${kernel} ]]; then
+    cd ${kernel_dir}
+    bmake depend
+    bmake -j$(nproc)
+    bmake install
+else
+    echo "Cannot compile kernel ${kernel} because ${kernel_dir} does not exist."
+fi
+
+cd ${prev_dir}
+echo "Done configuring and compiling the kernel"
