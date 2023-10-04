5f764b2999bbe01924b711edb102a4b1cc286d94
Devon Frind <devon.fri.2000@gmail.com>
2023-10-04 05:36:13 +0000

    Add clean build script and task

---
+++ scripts/clean_build.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+# Removes all generated files
+prev_dir=$(pwd)
+
+# Clean src directory
+if [[ -d ${OS161_SRC} ]]; then
+    echo "Cleaning src directory"
+    cd ${OS161_SRC}
+    bmake clean
+    bmake distclean
+else
+    echo "Cannot clean ${OS161_SRC}. It does not exist."
+fi
+
+# Clean kern/compile directories
+compile_directory=${OS161_SRC}/kern/compile
+if [[ -d ${compile_directory} ]]; then
+    cd ${compile_directory}
+    kernel_directories=$(ls -d *)
+
+    for subdir in ${kernel_directories}; do
+        echo "Cleaning ${subdir}"
+        cd ${compile_directory}/${subdir}
+        bmake clean
+    done
+else
+    echo "Cannot clean ${compile_directory}. It does not exist."
+fi
+
+cd ${prev_dir}
+
+echo "Clean done!"
